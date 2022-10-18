8a91873570ee450c50a5b1f32f5e1d00afe3a5e0
Kristina <111464160+kwilliams31@users.noreply.github.com>
2022-10-18 13:45:01 -0500

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,15 @@
-//
+$(function () {
+  let $z = $("<a id='newElement'>cats</a>");
+
+  $z.addClass("red")
+    .text("una")
+    .attr("href", "https://www.una.edu")
+    .attr("target", "_blank")
+    .attr("id", "betterID");
+
+  $("#containing-div").append($z);
+
+  console.log(typeof $z);
+
+  // $("body").append(typeof $z);
+});
